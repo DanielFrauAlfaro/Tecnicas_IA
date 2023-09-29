0b1b540f32dc8a34c88069abcf6fd28131b536cc
Daniel Frau Alfaro <danielfraualfa@gmail.com>
2023-09-29 12:57:50 +0200

    Comments: search possibles candidates to expand

--- Connect6Engine/search_engine.py
+++ Connect6Engine/search_engine.py
@@ -63,44 +63,77 @@ class SearchEngine():
         return True
         
     def find_possible_move(self):
-
-        possibles = []
-        seen = set(possibles)
-        n = 2
         
-        print("Recorrer")
+        # Lista de posibilidades
+        possibles = []
+
+        # Tabla Hash de la lista de posibilidades (consultas en O(1))
+        seen = set(possibles)
+
+        # N vecinos cercanos
+        n = 2
+
+        # Doble para recorrer todas las posiciones del tablero 
         for i in range(1, Defines.GRID_NUM - 1):
             for j in range(1, Defines.GRID_NUM - 1):
+
+                # Cambio de coordenadas de [1,1] a [19,1] propia del tablero 
                 x = Defines.GRID_NUM - 1 - j
                 y = i
                 stone = self.m_board[x][y]
-                        
+
+                # Si encuentra un hueco donde hay una ficha ...   
                 if stone is not Defines.NOSTONE:
+
+                    # Variable que indica que parte del cuadrado se incrementa: vertical o horizontal
+                    # Variable que indice la dirección en la que se buscan los vecinos
                     indices = 1
 
+                    # Recorre los n cuadrados de vecinos
                     for k in range(0,n):
+
+                        # Inicio de la ventana (coordenadas de esquina inferior izquierda - iteración)
+                        #   De fuera hacia dentro
                         start_xy = [x+n-k, y+n-k]
+
+                        # Incremento del recorrido
                         inc = -1
 
+
+                        # Para los cuatro lados del cuadrado
                         for h in range(0,4):
-
+                            
+                            # Para cada casilla de un lado (cada lado tiene 2 veces la distancia al vecino actual (n-k))
                             for f in range(0, 2*(n-k)):
-                                if (start_xy[0] < 20 and start_xy[0] > 0) and (start_xy[1] < 20 and start_xy[1] > 0):
 
+
+                                # TODO: ver que condición poner primero de las dos siguientes
+
+
+                                # Si la casilla explorada está dentro del tablero
+                                if (start_xy[0] < 20 and start_xy[0] > 0) and (start_xy[1] < 20 and start_xy[1] > 0):
+                                    
+                                    # Si la casilla a explorar es un hueco y no está en la lista de posibles (con la tabla Hash)
                                     if self.m_board[start_xy[0]][start_xy[1]] == Defines.NOSTONE and tuple([start_xy[0], start_xy[1]]) not in seen:
+
+                                        # Se añade a la lista de posibles y la tabla Hash
                                         possibles.append([start_xy[0], start_xy[1]])
                                         seen.add(tuple([start_xy[0], start_xy[1]]))
 
 
+                                # Intento de condición de cortar: alomejor faltan al explorar
                                 # elif inc < 0:
                                 #     start_xy[indices] += inc*(2*(n-k) - f+1)
                                 #     break
                                 
+                                # Incremento de la posición según la dirección (indices)
                                 start_xy[indices] += inc
                             
+                            # Cambia el incremento para hacer: hacia arriba (izq) - derecha (arriba) - abajo (dcha) - izquierda (abajo) 
                             if h == 1:
                                 inc = 1
                             
+                            # Cambia el índice para moverse: Y - X - (-Y) - (-X) --> el '-' es para la dirección que marca "indices"
                             if indices == 1:
                                 indices = 0
                             else:
